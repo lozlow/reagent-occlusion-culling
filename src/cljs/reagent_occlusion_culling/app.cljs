ee9606bea037a22a9590cde5420d30ce3e085be6
Pete Shaw <peeta.shaw@gmail.com>
2015-06-05 00:27:52 +0100

    - Adding the awesomeness

--- src/cljs/reagent_occlusion_culling/app.cljs
+++ src/cljs/reagent_occlusion_culling/app.cljs
@@ -2,22 +2,40 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [reagent-occlusion-culling.grid :as grid]))
 
-(defn some-component []
-  [:div
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red"]
-    " text."]])
-
 (defn reset-with-component-dimensions!
   [atom component]
   )
 
+(defn add-row-button
+  []
+  (let [input (atom nil)]
+  (fn []
+    [:div
+      [:input {:on-change #(reset! input (->> %
+                                              .-target
+                                              .-value))} @input]
+      [:button {:on-click #(swap! grid/grid-data conj {:id (+ (count @grid/grid-data) 1) :text @input})} "Add row"]])))
+
+(defn random-data-button
+  []
+  [:button {:on-click #(reset! grid/grid-data (grid/random-data 101))} "New random data"])
+
+(defn add-random-rows-button
+  []
+  (let [input (atom nil)]
+  (fn []
+    [:div
+      [:input {:on-change #(reset! input (->> %
+                                              .-target
+                                              .-value))} @input]
+      [:button {:on-click #(reset! grid/grid-data (into (grid/random-data (inc @input)) @grid/grid-data))} "Push number of rows"]])))
+
 (defn calling-component []
-  [:div "Parent component"
-   [some-component]
-   [grid/grid-container]])
+  [:div
+   [add-row-button]
+   [random-data-button]
+   [add-random-rows-button]
+   [grid/grid-container grid/grid-data]])
 
 (defn init []
   (reagent/render-component [calling-component]
--- src/cljs/reagent_occlusion_culling/grid.cljs
+++ src/cljs/reagent_occlusion_culling/grid.cljs
@@ -1,36 +1,44 @@
 (ns reagent-occlusion-culling.grid
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.string :refer [split]]))
 
 (defn grid-table-data
   [data]
   [:tbody
-    (for [item data]
-      ^{:key (:id item)}
-      [:tr
-        [:td (:id item)]
-        [:td (:text item)]])])
+    (for [item data
+          :let [classname (if (even? (:idx item)) "Grid__row--even" "Grid__row--odd")]]
+      ^{:key (:idx item)}
+      [:tr.Grid__row {:class classname}
+        [:td.Grid__cell (:id item)]
+        [:td.Grid__cell (:text item)]])])
+
+(def paragraph
+  (split "Aliquam vitae congue ipsum. Pellentesque varius, est sed feugiat eleifend, justo purus bibendum mi, a interdum ligula felis eu tellus. Aenean nec dui molestie, mollis nisi ac, placerat diam. Ut a nibh dictum, tincidunt erat eget, luctus metus. Vivamus maximus justo nec sapien commodo, non ullamcorper velit congue. Donec vestibulum mauris sed metus gravida, efficitur lobortis ex dictum. Ut imperdiet elementum imperdiet. Donec mollis ac arcu vel dictum. Etiam vehicula pretium ligula, ac rhoncus erat hendrerit pharetra. Vestibulum sed elit ut massa fringilla venenatis nec ut justo. Quisque luctus dolor nec urna aliquam, at ullamcorper dui semper. Mauris sagittis sapien."
+         " "))
+
+(defn random-data
+  [count]
+  (vec (map (fn [item] {:id item :text (rand-nth paragraph)}) (range 1 count))))
+
+(def grid-data
+  (atom (random-data 101)))
 
 (defn grid-container
-  []
+  [data]
   (let [row-height 20
-        row-data '[{:id 1 :text "test"} {:id 2 :text "one"} {:id 3 :text "389hw"} {:id 4 :text "328hg"}
-                   {:id 5 :text "test"} {:id 6 :text "one"} {:id 7 :text "389hw"} {:id 8 :text "328hg"}
-                   {:id 9 :text "test"} {:id 10 :text "one"} {:id 11 :text "389hw"} {:id 12 :text "328hg"}]
-        full-height (* row-height (count row-data))
         top (atom 0)
         grid-height 150]
-    (fn []
-      (let [first-item (bit-or (/ @top row-height) 0)
-            last-item (min (count row-data) (+ first-item (bit-or (/ grid-height row-height) 0) 2))]
+    (fn [data]
+      (let [data (vec (map-indexed #(assoc %2 :idx (inc %)) @data))
+            first-item (bit-or (/ @top row-height) 0)
+            full-height (* row-height (count data))
+            last-item (min (count data) (+ first-item (bit-or (/ grid-height row-height) 0) 2))]
       [:div
-        [:span first-item]
-        [:span last-item]
+        [:span (str "Showing row indices " (inc first-item) " to " last-item)]
         [:div.Grid {:style {:height grid-height
                             :overflow "auto"}
-                    :on-scroll #(reset! top (->> %
-                                                 .-target
-                                                 .-scrollTop))}
+                    :on-scroll #(reset! top (->> % .-target .-scrollTop))}
           [:div.Grid__wrapper {:style {:height full-height}}
             [:table.Grid__table {:style {:position :relative
                                          :top @top}}
-              [grid-table-data (subvec row-data first-item last-item)]]]]]))))
+              [grid-table-data (subvec data first-item last-item)]]]]]))))
